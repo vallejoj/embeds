6559a5c743991186d5ea4b439c7ccf0884d00205
Joshi Vallejo <josvalle@Joshis-MacBook-Pro.local>
2018-12-11 15:43:08 -0800

    hello

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`my name is Joshiio`);
 });
